6994289eccc6600ded4e23033a5b8c31097ae77a
Anniejs <anniejs@163.com>
2018-12-27 20:51:09 +0800

    导航守卫功能提交

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ Vue.use(Router)
   1. 给某个路由规则，使用children属性，可以配置当前路由的子路由
   2. app.vue中router-view,顶层出口， 还需要在当前组件中使用router-view,用于显示子路由
 */
-export default new Router({
+const router = new Router({
   routes: [
 
     {
@@ -34,3 +34,23 @@ export default new Router({
     }
   ]
 })
+// 给router对象注册导航守卫
+// to: 要去哪儿
+// from: 从哪儿来
+// next: 是否放行   next(): 方向  next('/login')  next('/home')
+router.beforeEach((to, from, next) => {
+  // 判断如果是去登录就放行
+  if (to.path === '/login') {
+    next()
+    return
+  }
+  // 如果不是去登录，就判断有没有token，如果有就放行
+  // 如果没有token就去登录
+  let token = localStorage.getItem('token')
+  if (token) {
+    next()
+  } else {
+    next('/login')
+  }
+})
+export default router
